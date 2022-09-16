fc4803d846669d24ac3b331d309d5403a6c54465
Antoine Albertelli <antoinealb@google.com>
2022-09-16 20:14:17 +0200

    Provide a simple index view which shows the score

--- championship/views.py
+++ championship/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Player, compute_scores
 
-# Create your views here.
+
+def index(request):
+    players = list(Player.objects.all())
+    scores_by_player = compute_scores()
+    for p in players:
+        p.score = scores_by_player[p.id]
+    players.sort(key=lambda l: l.score, reverse=True)
+    return render(request, "championship/index.html", dict(players=players))
