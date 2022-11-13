719114b03d08dab9cd71f0c06f944faa5811c129
Antoine Albertelli <antoinealb@google.com>
2022-11-13 21:15:28 +0100

    Fix a warning about leading slash in URLs

--- championship/urls.py
+++ championship/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/ranking", views.ranking, name="ranking"),
+    path("ranking", views.ranking, name="ranking"),
     path("player/<int:player_id>/", views.player_details, name="player_details"),
 ]
