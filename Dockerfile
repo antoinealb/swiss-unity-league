44bc4db0d3f49abfff8333a79a1a4dfe225ed3e7
Antoine Albertelli <antoinealb@google.com>
2022-11-13 22:59:43 +0100

    Add a few things to collect static files

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,9 @@ RUN pip install --upgrade pip
 COPY requirements.txt /app
 RUN pip install -r requirements.txt
 
-COPY . $DockerHOME
+COPY . /app
+
+RUN /app/manage.py collecstatic
 
 EXPOSE 8000
 
--- mtg_championship_site/settings.py
+++ mtg_championship_site/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,6 +130,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
