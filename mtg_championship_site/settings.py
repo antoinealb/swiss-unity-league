221c3b936c8b269d0e18b5bf433429e547dfdc4d
Antoine Albertelli <antoinealb@google.com>
2022-11-13 23:09:36 +0100

    Add a favicon

--- mtg_championship_site/settings.py
+++ mtg_championship_site/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "static_root"
 
+WHITENOISE_ROOT = STATIC_ROOT / "root"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
