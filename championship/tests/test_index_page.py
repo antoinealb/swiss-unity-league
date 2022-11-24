689aac619f8235a23cc015786c585a91e7be7ac9
Antoine Albertelli <antoinealb@google.com>
2022-11-24 15:20:08 +0100

    Add basic testing for the home page
    Those would have caught the two regressions caused by switching to class
    views, so its probably worth having them.
    
    Regression commits:
    
    bf23c84fd5db7f8708ef47efe4ee5ce86dc6a6e2
    4d1a86069f775679667ef19d9ce907648e46e80e

---
+++ championship/tests/test_index_page.py
@@ -0,0 +1,35 @@
+import datetime
+from django.test import TestCase, Client
+from django.urls import reverse
+from faker import Faker
+from championship.models import Event, EventOrganizer
+from championship.factories import *
+
+
+class AdminViewTestCase(TestCase):
+    """
+    Tests for the landing page of the website.
+    """
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_shows_title_of_next_event(self):
+        """
+        Checks that the home page contains a list of coming up events.
+        """
+        d = datetime.date.today() + datetime.timedelta(days=1)
+        event = EventFactory(name="TestEvent2000", date=d)
+
+        response = self.client.get("/")
+
+        self.assertIn("TestEvent2000", response.content.decode())
+
+    def test_shows_player_name(self):
+        """
+        Checks that the homepage contains some player information.
+        """
+        player = PlayerFactory()
+        response = self.client.get("/")
+        self.assertIn(player.first_name, response.content.decode())
+        self.assertIn(player.last_name, response.content.decode())
