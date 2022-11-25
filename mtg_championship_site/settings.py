2c4e408523d7953de224c09d83463049c927fb5a
Antoine Albertelli <antoinealb@google.com>
2022-11-25 11:28:35 +0100

    Add the login views

--- mtg_championship_site/settings.py
+++ mtg_championship_site/settings.py
@@ -143,3 +143,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
