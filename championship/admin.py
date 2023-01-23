ba9d103d760a2e9cc594f1a1dc151302274bb870
Antoine Albertelli <antoinealb@google.com>
2023-01-23 16:48:34 +0100

    Implement an admin action to merge two players
    It happens that players eventually end up on the league website under
    two different names, for example because two tournament organizers use
    different convention, or someone uses a nickname or something.
    
    In those cases, we would like to have the ability to regroup all results
    under a single Player, discarding extra Player objects. This action
    gives us the possibility to easily do that from the admin panel, showing
    us a nice confirmation screen to make sure we do not mess up anything.

--- championship/admin.py
+++ championship/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import *
 
 
@@ -28,6 +30,43 @@ class PlayerAdmin(admin.ModelAdmin):
     inlines = [ResultInline]
     search_fields = ["name"]
     list_display = ["name"]
+    actions = ["merge_players"]
+
+    @admin.action(
+        description="Merge selected players, keeping the oldest one.",
+        permissions=["delete"],
+    )
+    def merge_players(self, request, queryset):
+        players = list(queryset.order_by("id"))
+        original_player = players[0]
+
+        if "post" in request.POST:
+            for player in players[1:]:
+                for e in EventPlayerResult.objects.filter(player=player):
+                    e.player = original_player
+                    e.save()
+                player.delete()
+
+            return redirect(
+                "admin:championship_player_change",
+                object_id=original_player.id,
+            )
+
+        else:
+            results = sum(
+                (list(EventPlayerResult.objects.filter(player=p)) for p in players), []
+            )
+
+            return render(
+                request,
+                "admin/merge_confirmation.html",
+                context={
+                    "players": players,
+                    "results": results,
+                    "original_player": original_player,
+                    "action": request.POST["action"],
+                },
+            )
 
 
 admin.site.register(Player, PlayerAdmin)
