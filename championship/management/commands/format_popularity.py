f75577506b42c70f9dfc9f43ee083e28ec81fed0
Antoine Albertelli <antoinealb@google.com>
2023-02-23 00:09:09 +0100

    Add a command to display stats about format popularity

---
+++ championship/management/commands/format_popularity.py
@@ -0,0 +1,35 @@
+import itertools
+from difflib import SequenceMatcher
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+
+from prettytable import PrettyTable
+from championship.models import *
+
+
+class Command(BaseCommand):
+    help = "Report on number of player and events by format"
+
+    def handle(self, *args, **kwargs):
+        players_by_format = []
+
+        table = PrettyTable(field_names=["Format", "Player Count"], align="l")
+        for entry in (
+            Event.objects.all()
+            .values("format")
+            .annotate(player_count=Count("player"))
+            .order_by("-player_count")
+        ):
+            table.add_row((entry["format"], entry["player_count"]))
+        print(table)
+
+        table = PrettyTable(field_names=["Format", "Event Count"], align="l")
+        for entry in (
+            Event.objects.all()
+            .values("format")
+            .annotate(event_count=Count("id"))
+            .order_by("-event_count")
+        ):
+            table.add_row((entry["format"], entry["event_count"]))
+        print(table)
