bd7514f684308ac327617ee17eb69b5c9b0e8231
Antoine Albertelli <antoinealb@google.com>
2023-05-29 17:33:14 +0200

    reformat

--- invoicing/urls.py
+++ invoicing/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-        path("", views.InvoiceList.as_view(), name="invoice_list"),
-        path("<int:pk>/", views.RenderInvoice.as_view(), name="invoice_get"),
+    path("", views.InvoiceList.as_view(), name="invoice_list"),
+    path("<int:pk>/", views.RenderInvoice.as_view(), name="invoice_get"),
 ]
