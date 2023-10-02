8f1e0544872ac6940b87f24ee3e1a1122f56eaa3
Jari Rentsch <jari.rentsch@jkweb.ch>
2023-10-02 17:06:26 +0200

    Implement excel parser with match points

--- championship/parsers/excel_parser.py
+++ championship/parsers/excel_parser.py
@@ -1,24 +1,44 @@
+import math
 import pandas as pd
 from championship.parsers.general_parser_functions import parse_record, record_to_points
 
-
-RANK = "RANK"
 PLAYER_NAME = "PLAYER_NAME"
 RECORD = "RECORD"
 MATCH_POINTS = "MATCH_POINTS"
 
 
+def _estimate_rounds(match_point_list):
+    num_players = len(match_point_list)
+
+    # The number of rounds needs to be at least the amount wins + draws of an individual player
+    min_num_rounds = max([mp // 3 + mp % 3 for mp in match_point_list])
+
+    # We add +1 because the actual rounds is likely to be 1 higher
+    byes = min_num_rounds + 1 if num_players % 2 == 1 else 0
+
+    total_wins = sum([mp // 3 for mp in match_point_list]) - byes
+    total_losses = total_wins
+    total_draws = sum([mp % 3 for mp in match_point_list])
+
+    # We can estimate the number of rounds played based on the total number of wins, losses, draws and byes
+    number_of_matches_per_player = (
+        total_wins + total_draws + total_losses + byes
+    ) / num_players
+
+    # We round it up, since some players might drop out
+    rounds_estimate = math.ceil(number_of_matches_per_player)
+
+    return max(rounds_estimate, min_num_rounds)
+
+
 def _standings(df: pd.DataFrame):
     defined_cols = [
-        col for col in [RANK, PLAYER_NAME, RECORD, MATCH_POINTS] if col in df.columns
+        col for col in [PLAYER_NAME, RECORD, MATCH_POINTS] if col in df.columns
     ]
     df = df[defined_cols]
-    if RANK not in defined_cols:
-        df[RANK] = df.index + 1
+    
     if PLAYER_NAME not in defined_cols:
         raise PlayerNameNotFound()
-    if RECORD not in defined_cols and MATCH_POINTS not in defined_cols:
-        raise RecordOrMatchPointsNotFound()
 
     if RECORD in defined_cols:
         for _, row in df.iterrows():
@@ -30,30 +50,58 @@ def _standings(df: pd.DataFrame):
             except ValueError:
                 raise InvalidRecordError(name, record_string)
             yield (name, points, tuple(parsed_record))
+    elif MATCH_POINTS in defined_cols:
+        match_point_list = []
+        for _, row in df.iterrows():
+            try:
+                match_points = int(row[MATCH_POINTS])
+                match_point_list.append(match_points)
+            except:
+                raise InvalidMatchPointsError(row[PLAYER_NAME], row[MATCH_POINTS])
+        num_rounds = _estimate_rounds(match_point_list)
+        for i, row in df.iterrows():
+            name = row[PLAYER_NAME]
+            points = match_point_list[i]
+            wins = points // 3
+            draws = points % 3
+            losses = num_rounds - wins - draws
+            yield (name, points, (wins, losses, draws))
+    else:
+        raise RecordOrMatchPointsNotFound()
 
 
 def parse_standings_page(df: pd.DataFrame):
     standings = list(_standings(df))
-    return standings
+    #Sort by match points
+    sorted_standings = sorted(standings, key=lambda x: x[1], reverse=True)
+    return sorted_standings
 
 
 class PlayerNameNotFound(ValueError):
-    def __init__(self, message="Column PLAYER_NAME not found", *args, **kwargs):
+    def __init__(self, message=f"Column {PLAYER_NAME} not found", *args, **kwargs):
         super().__init__(message, *args, **kwargs)
 
-    ui_error_message = "The column PLAYER_NAME was not found in the excel file. Please make sure to specify the first and lastname of each player in the column PLAYER_NAME."
+    ui_error_message = f"The column {PLAYER_NAME} was not found in the excel file. Please make sure to specify the first and lastname of each player in the column {PLAYER_NAME}."
 
 
 class RecordOrMatchPointsNotFound(ValueError):
     def __init__(
-        self, message="Column RECORD or MATCH_POINTS not found", *args, **kwargs
+        self, message=f"Column {RECORD} or {MATCH_POINTS} not found", *args, **kwargs
     ):
         super().__init__(message, *args, **kwargs)
 
-    ui_error_message = "The column RECORD or MATCH_POINTS was not found in the excel file. Please make sure to specify either the record or match points of each player in the column RECORD or MATCH_POINTS."
+    ui_error_message = f"The column {RECORD} or {MATCH_POINTS} was not found in the excel file. Please make sure to specify either the record or match points of each player in the column {RECORD} or {MATCH_POINTS}."
 
 
 class InvalidRecordError(ValueError):
     def __init__(self, player_name, record, message="Invalid record", *args, **kwargs):
         super().__init__(message, *args, **kwargs)
         self.ui_error_message = f"The record {record} of player {player_name} is invalid. Please make sure to specify the record in the format W-L-D."
+
+
+class InvalidMatchPointsError(ValueError):
+    def __init__(
+        self, player_name, match_points, message="Invalid match points", *args, **kwargs
+    ):
+        super().__init__(message, *args, **kwargs)
+        self.ui_error_message = f"The match points {match_points} of player {player_name} are invalid. Please make sure to specify the match points as an integer."
--- championship/tests/parsers/test_excel.py
+++ championship/tests/parsers/test_excel.py
@@ -1,14 +1,13 @@
-from championship.parsers import excel_parser
+from championship.parsers.excel_parser import *
 from unittest import TestCase
 import pandas as pd
+from parameterized import parameterized
 
 
 class ExcelStandingParser(TestCase):
-    def setUp(self):
+    def test_can_parse_record(self):
         self.df = pd.read_excel("championship/tests/parsers/excel_ranking.xlsx")
-        self.results = excel_parser.parse_standings_page(self.df)
-
-    def test_can_parse(self):
+        self.results = parse_standings_page(self.df)
         want = [
             ("Jari Rentsch", 18, (6, 1, 0)),
             ("Noé Dumez", 17, (5, 0, 2)),
@@ -16,23 +15,82 @@ class ExcelStandingParser(TestCase):
         ]
         self.assertEqual(want, self.results[:3])
 
+    def _create_df(self, match_points):
+        self.df = pd.DataFrame(
+            {
+                PLAYER_NAME: [f"Player {i}" for i in range(1, len(match_points) + 1)],
+                MATCH_POINTS: match_points,
+            }
+        )
+
+    @parameterized.expand(
+        [
+            ([9, 9, 9, 9, 7, 7, 6, 6, 3, 3, 3], [
+            (3, 1, 0),
+            (3, 1, 0),
+            (3, 1, 0),
+            (3, 1, 0),
+            (2, 1, 1),
+            (2, 1, 1),
+            (2, 2, 0),
+            (2, 2, 0),
+            (1, 3, 0),
+            (1, 3, 0),
+            (1, 3, 0),
+        ]),
+            ([10, 9, 9, 7, 7, 7, 6, 6, 3, 3, 3], [
+            (3, 0, 1),
+            (3, 1, 0),
+            (3, 1, 0),
+            (2, 1, 1),
+            (2, 1, 1),
+            (2, 1, 1),
+            (2, 2, 0),
+            (2, 2, 0),
+            (1, 3, 0),
+            (1, 3, 0),
+            (1, 3, 0),
+        ])])
+    def test_can_parse_match_points(self, match_points, want):
+        self._create_df(match_points)
+        self.results = parse_standings_page(self.df)
+        records = [record for _,_,record in self.results]
+        self.assertEqual(want, records)
+   
+    def test_ranking(self):
+        self.df = pd.read_excel("championship/tests/parsers/excel_ranking.xlsx")
+        # Reverse the order of the dataframe
+        self.df = self.df.iloc[::-1]
+        self.results = parse_standings_page(self.df)
+        want = [
+            ("Jari Rentsch", 18, (6, 1, 0)),
+            ("Noé Dumez", 17, (5, 0, 2)),
+            ("RENAUD-GOUD, Antoine", 16, (5, 1, 1)),
+        ]
+        self.assertEqual(want, self.results[:3])
 
 class ExcelStandingParserExceptions(TestCase):
     def setUp(self):
         self.df = pd.read_excel("championship/tests/parsers/excel_ranking.xlsx")
 
     def test_player_name_not_found(self):
-        del self.df["PLAYER_NAME"]
-        with self.assertRaises(excel_parser.PlayerNameNotFound):
-            excel_parser.parse_standings_page(self.df)
+        del self.df[PLAYER_NAME]
+        with self.assertRaises(PlayerNameNotFound):
+            parse_standings_page(self.df)
 
     def test_record_or_match_points_not_found(self):
-        del self.df["RECORD"]
-        del self.df["MATCH_POINTS"]
-        with self.assertRaises(excel_parser.RecordOrMatchPointsNotFound):
-            excel_parser.parse_standings_page(self.df)
+        del self.df[RECORD]
+        del self.df[MATCH_POINTS]
+        with self.assertRaises(RecordOrMatchPointsNotFound):
+            parse_standings_page(self.df)
 
     def test_invalid_record(self):
-        self.df.loc[0, "RECORD"] = "invalid"
-        with self.assertRaises(excel_parser.InvalidRecordError):
-            excel_parser.parse_standings_page(self.df)
+        self.df.loc[0, RECORD] = "invalid"
+        with self.assertRaises(InvalidRecordError):
+            parse_standings_page(self.df)
+
+    def test_invalid_match_points(self):
+        del self.df[RECORD]
+        self.df.loc[0, MATCH_POINTS] = "invalid"
+        with self.assertRaises(InvalidMatchPointsError):
+            parse_standings_page(self.df)
