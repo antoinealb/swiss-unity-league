048e752848a468cc1bec41fc5d59b068c4fa0b82
Antoine Albertelli <antoinealb@google.com>
2023-10-25 16:17:00 +0200

    Add a string representation to EPR

--- championship/models.py
+++ championship/models.py
@@ -338,6 +338,10 @@ class EventPlayerResult(models.Model):
     class Meta:
         indexes = [models.Index(fields=["event"])]
 
+    def __str__(self):
+        score = f"{self.win_count}-{self.loss_count}-{self.draw_count}"
+        return f"{self.player.name}@{self.event.name} ({score})"
+
     def __lt__(self, other):
         """Comparison function for sorting.
 
--- championship/tests/models/test_eventplayerresult.py
+++ championship/tests/models/test_eventplayerresult.py
@@ -6,3 +6,10 @@ class EventPlayerResultFactoryTest(TestCase):
     def test_score_is_computed_automatically(self):
         p = EventPlayerResultFactory(win_count=3, draw_count=1)
         self.assertEqual(10, p.points)
+
+class EventPlayerResultTest(TestCase):
+    def test_str(self):
+        p = EventPlayerResultFactory(win_count=3, draw_count=0, loss_count=2)
+        self.assertEqual(
+            f"{p.player.name}@{p.event.name} (3-2-0)",
+            str(p))
