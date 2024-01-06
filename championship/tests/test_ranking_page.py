0d6511a6ac0427168ec9099d8e5348ddfc16cbac
Antoine Albertelli <antoinealb@google.com>
2024-01-06 15:11:36 -0500

    Make the ranking view per-year
    This view can now take season slugs as a parameters, but does not expose
    them as different buttons yet. In order to make it available from the
    user website, I would like to first make sure the score computation is
    correct.

--- championship/tests/test_ranking_page.py
+++ championship/tests/test_ranking_page.py
@@ -23,21 +23,32 @@ class RankingTestCase(TestCase):
         player_with_results = PlayerFactory()
         EventPlayerResultFactory(player=player_with_results, points=1)
         player_without_results = PlayerFactory()
-        response = self.client.get("/ranking")
+        response = self.client.get("/ranking/2023")
         self.assertContains(response, player_with_results.name)
         self.assertNotContains(response, player_without_results.name)
 
     def test_ranking_for_player_hidden(self):
         """Checks that we hide hidden players from the leaderboard."""
         player = PlayerFactory(hidden_from_leaderboard=True)
+        event = EventFactory(date=datetime.date(2023, 4, 1))
         EventPlayerResultFactory(player=player, points=1)
-        response = self.client.get("/ranking")
+        response = self.client.get("/ranking/2023")
         self.assertNotContains(response, player.name)
 
     def test_score_properties_rendering(self):
         """Checks that the score properties are rendered correctly."""
         player = PlayerFactory()
-        EventPlayerResultFactory(player=player, points=2)
-        response = self.client.get("/ranking")
+        event = EventFactory(date=datetime.date(2023, 4, 1))
+        EventPlayerResultFactory(player=player, points=2, event=event)
+        response = self.client.get("/ranking/2023")
         self.assertContains(response, """<i class="icon-star"></i>""")
         self.assertContains(response, """<i class="icon-shield"></i>""")
+
+    def test_get_for_default_season(self):
+        """Checks that we can get the page for the default season without a crash."""
+        response = self.client.get("/ranking")
+        self.assertEqual(200, response.status_code)
+
+    def test_ranking_for_unknown_season_returns_404(self):
+        response = self.client.get("/ranking/2022")
+        self.assertEqual(404, response.status_code)
