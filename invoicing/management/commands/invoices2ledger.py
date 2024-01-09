4092b1667f2b2e0dc59c79a429d6126c82f9198a
Antoine Albertelli <antoinealb@google.com>
2024-01-09 11:37:27 +0100

    Export invoices to accounting software

---
+++ invoicing/management/commands/invoices2ledger.py
@@ -0,0 +1,26 @@
+import datetime
+import textwrap
+
+from django.core.management.base import BaseCommand
+
+from invoicing.models import Invoice
+
+
+class Command(BaseCommand):
+    help = "Convert all paid invoices into a format that can be understood by https://ledger-cli.org/."
+
+    def handle(self, *args, **kwargs):
+        for invoice in (
+            Invoice.objects.exclude(payment_received_date__isnull=True)
+            .select_related("event_organizer")
+            .order_by("payment_received_date", "id")
+        ):
+            print(
+                textwrap.dedent(
+                    f"""\
+            {invoice.payment_received_date.strftime('%Y-%m-%d')} {invoice.event_organizer.name} # { invoice.reference }
+                Assets:Bank  CHF{invoice.total_amount}
+                Income:SUL Fees
+            """
+                )
+            )
