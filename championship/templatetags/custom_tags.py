a25db07572a698242260bbaf30c1e2c4c5241622
Antoine Albertelli <antoinealb@google.com>
2024-01-12 12:48:07 +0100

    Replace custom tag by a single if in template code
    Let's reserve template tags for things that cannot simply be expressed
    in templates, because they tend to make the code harder to read (need to
    switch between the Python code and template to see what's up).

--- championship/templatetags/custom_tags.py
+++ championship/templatetags/custom_tags.py
@@ -12,12 +12,3 @@ def rangeFilter(value):
 @register.simple_tag
 def verbose_name(model):
     return model._meta.verbose_name
-
-
-@register.simple_tag
-def season_url(view_name, slug, pk=None):
-    """Needed to allow the season dropdown to have an optional url argument called pk."""
-    if pk:
-        return reverse(view_name, kwargs={"slug": slug, "pk": pk})
-    else:
-        return reverse(view_name, kwargs={"slug": slug})
