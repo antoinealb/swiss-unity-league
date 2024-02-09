c80df1650dd10d6918f1d197ca7a032c127ddc1a
Antoine Albertelli <antoinealb@google.com>
2024-02-09 16:25:32 +0100

    Store file operations in log

--- file_storage_db/models.py
+++ file_storage_db/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from auditlog.registry import auditlog
 
 
 class File(models.Model):
@@ -8,3 +9,6 @@ class File(models.Model):
 
     def get_absolute_url(self):
         return reverse("file_db_serve", args=[self.filename])
+
+
+auditlog.register(File)
