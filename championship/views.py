95df24c13147c6edfb823de86ee6fcddd9f1d789
Jari Rentsch <jari.rentsch@jkweb.ch>
2024-02-14 20:06:20 +0100

    Update headers to mock chrome

--- championship/views.py
+++ championship/views.py
@@ -645,8 +645,16 @@ class CreateLinkParserResultsView(LoginRequiredMixin, CreateResultsView):
             return
         try:
             headers = {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                 "Accept-Language": "en-US,en;q=0.9",
+                "Connection": "keep-alive",
+                "Cache-conrol": "max-age=0",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+                "Upgrade-Insecure-Requests": "1",
             }
             response = requests.get(url, headers=headers)
             response.raise_for_status()
