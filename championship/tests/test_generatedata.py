a28470803e3946203c386ba98cd13e88b5dd06e0
Antoine Albertelli <antoinealb@google.com>
2024-03-01 16:08:45 +0100

    fixup

--- championship/tests/test_generatedata.py
+++ championship/tests/test_generatedata.py
@@ -7,4 +7,3 @@ class GenerateFakeDataTest(TestCase):
     def test_generate_data(self):
         call_command("generatedata", players_count=4, events_count=2)
         self.assertEqual(4, Player.objects.all().count())
-        self.assertEqual(2, Event.objects.all().count())
