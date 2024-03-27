d4a335672a646d0ad5c98b4182c1a3a4a3e79d47
Jari Rentsch <jari.rentsch@jkweb.ch>
2024-03-27 14:10:47 +0100

    Score computation for SUL Trial

--- championship/score/trial_2024.py
+++ championship/score/trial_2024.py
@@ -1,6 +1,44 @@
+from dataclasses import dataclass
+
+from championship.models import Count, Event
 from championship.score.season_2024 import ScoreMethod2024
+from championship.score.types import LeaderboardScore, QualificationType
+from championship.season import SEASON_2024, SUL_TRIAL_2024
 
 
 class ScoreMethodTrial2024(ScoreMethod2024):
+    TOTAL_QUALIFICATION_SLOTS = 80
 
-    pass
+    @classmethod
+    def finalize_scores(
+        cls,
+        scores_by_player: dict[int, ScoreMethod2024.Score],
+    ) -> dict[int, LeaderboardScore]:
+        """Implements the last step of score processing.
+
+        This function takes a list of (player_id, score) tuples and turns it
+        into a sequence of Score objects, checking the maximum number of byes
+        and deciding who is qualified and not.
+
+        Returns a dict of (player_id: Score)
+
+        """
+
+        num_leaderboard_qualifications = cls.TOTAL_QUALIFICATION_SLOTS
+        sorted_scores = sorted(
+            scores_by_player.items(), key=lambda x: x[1].qps, reverse=True
+        )
+        scores = {}
+        for i, (player_id, score) in enumerate(sorted_scores):
+            rank = i + 1
+
+            scores[player_id] = LeaderboardScore(
+                total_score=score.qps,
+                rank=rank,
+            )
+            if num_leaderboard_qualifications > 0:
+                scores[player_id].qualification_type = QualificationType.LEADERBOARD
+                scores[player_id].qualification_reason = "Qualified for SUL Trial 2024"
+                num_leaderboard_qualifications -= 1
+
+        return scores
---
+++ championship/tests/score/test_score_trial_2024.py
@@ -0,0 +1,64 @@
+from django.test import TestCase
+
+from championship.factories import *
+from championship.models import *
+from championship.score import compute_scores
+from championship.score.trial_2024 import ScoreMethodTrial2024
+from championship.score.types import QualificationType
+from championship.season import SUL_TRIAL_2024
+
+
+def create_test_tournament(players, category=Event.Category.PREMIER, with_top8=True):
+    event = Event2024Factory(category=category)
+    num_players = len(players)
+    for i, player in enumerate(players):
+        rank = i + 1
+
+        if category != Event.Category.REGULAR and with_top8:
+            if rank == 1:
+                ser = EventPlayerResult.SingleEliminationResult.WINNER
+            elif rank == 2:
+                ser = EventPlayerResult.SingleEliminationResult.FINALIST
+            elif rank <= 4:
+                ser = EventPlayerResult.SingleEliminationResult.SEMI_FINALIST
+            elif rank <= 8:
+                ser = EventPlayerResult.SingleEliminationResult.QUARTER_FINALIST
+            else:
+                ser = None
+
+        EventPlayerResultFactory(
+            player=player,
+            points=num_players - i,
+            ranking=rank,
+            single_elimination_result=ser,
+            event=event,
+        )
+    return event
+
+
+class TestScoresQualified(TestCase):
+    def setUp(self):
+        self.num_qualified = ScoreMethodTrial2024.TOTAL_QUALIFICATION_SLOTS
+
+    def compute_scores(self):
+        return compute_scores(SUL_TRIAL_2024)
+
+    def test_top_leaderboard_qualified(self):
+        num_players = 100
+        players = [PlayerFactory() for _ in range(num_players)]
+
+        create_test_tournament(players, category=Event.Category.PREMIER)
+        got_qualified = [s.qualification_type for s in self.compute_scores().values()]
+        want_qualified = [QualificationType.LEADERBOARD] * (self.num_qualified) + [
+            QualificationType.NONE
+        ] * (num_players - self.num_qualified)
+        self.assertEqual(want_qualified, got_qualified)
+
+    def test_top_leaderboard_no_byes(self):
+        num_players = 100
+        players = [PlayerFactory() for _ in range(num_players)]
+
+        create_test_tournament(players, category=Event.Category.PREMIER)
+        got_scores = self.compute_scores()
+        got_byes = [s.byes for s in got_scores.values()]
+        self.assertEqual([0] * num_players, got_byes)
