181eb453b1d434aa1a937fccd352a629d214c7ff
Antoine Albertelli <antoinealb@google.com>
2024-04-08 09:35:07 +0200

    Define decklist-related models
    This commit adds the models discussed in docs/design_decklists.md and is
    the first step towards actually collecting decklists on UnityLeague.

---
+++ decklists/models.py
@@ -0,0 +1,74 @@
+# Copyright 2024 Leonin League
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import uuid
+
+from django.db import models
+from django.utils.timezone import now
+
+from championship.models import Event, Player
+
+
+class Collection(models.Model):
+    """Group of decklists that are collected at the same time and for the same purpose.
+
+    For example, one group could be "Decklists for the Modern portion of the 2024 trial".
+    """
+
+    name = models.CharField(
+        help_text="Human readable name of the Collection", max_length=128
+    )
+    submission_deadline = models.DateTimeField(
+        help_text="Time until new decklists can be created for this group or existing ones can be edited."
+    )
+    publication_time = models.DateTimeField(
+        help_text="Time at which the decklists will be revealed."
+    )
+    event = models.ForeignKey(
+        Event,
+        help_text="Event for which those decklists are.",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} (by {self.event.organizer.name})"
+
+    @property
+    def decklists_published(self):
+        return now() > self.publication_time
+
+
+class Decklist(models.Model):
+    """A single deck, for a single player, in a single Colection."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    collection = models.ForeignKey(Collection, on_delete=models.RESTRICT)
+    player = models.ForeignKey(
+        Player, help_text="Who is playing this deck.", on_delete=models.RESTRICT
+    )
+    archetype = models.CharField(
+        help_text="Player-submitted name for the deck (e.g. 'Burn')", max_length=64
+    )
+    last_modified = models.DateTimeField(
+        help_text="Last modification timestamp.", auto_now=True
+    )
+    mainboard = models.TextField(
+        help_text="Content of the main deck, one entry per line (e.g. 4 Brainstorm)"
+    )
+    sideboard = models.TextField(
+        help_text="Content of the sideboard, also one entry per line"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.player.name} ({self.archetype})"
---
+++ decklists/tests/test_models.py
@@ -0,0 +1,71 @@
+# Copyright 2024 Leonin League
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+
+from django.test import TestCase
+from django.utils.timezone import now
+
+from championship.factories import EventFactory, EventOrganizerFactory, PlayerFactory
+from decklists.factories import CollectionFactory, DecklistFactory
+from decklists.models import Collection, Decklist
+
+
+class CollectionTest(TestCase):
+    def test_str(self):
+        eo = EventOrganizerFactory(name="Leonin League")
+        event = EventFactory(organizer=eo)
+        collection = Collection(
+            name="Foobar",
+            submission_deadline=datetime.datetime.now(),
+            event=event,
+        )
+
+        want = "Foobar (by Leonin League)"
+        self.assertEqual(str(collection), want)
+
+    def test_factory(self):
+        collection = CollectionFactory()
+        self.assertIn(collection.event.organizer.name, str(collection))
+
+    def test_published(self):
+        collection = CollectionFactory()
+        self.assertFalse(collection.decklists_published)
+
+        collection = CollectionFactory(
+            publication_time=now() - datetime.timedelta(hours=1)
+        )
+        self.assertTrue(collection.decklists_published)
+
+
+class DecklistTest(TestCase):
+    def test_str(self):
+        player = PlayerFactory(name="Antoine Albertelli")
+        decklist = DecklistFactory(player=player, archetype="Burn")
+
+        want = "Antoine Albertelli (Burn)"
+        self.assertEqual(str(decklist), want)
+
+    def test_factory(self):
+        dl = DecklistFactory()
+        self.assertIsNotNone(dl.id)
+
+    def test_decklists_ids_are_all_unique(self):
+        n = 10
+        collection = CollectionFactory()
+        player = PlayerFactory()
+        ids = set(
+            DecklistFactory(collection=collection, player=player).id for _ in range(n)
+        )
+        self.assertEqual(n, len(ids))
