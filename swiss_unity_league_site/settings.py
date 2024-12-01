fa173949642abcae14db1866106992e3bbe38500
Antoine Albertelli <antoinealb@google.com>
2024-12-01 22:53:34 +0100

    Time to start a new season :)

--- swiss_unity_league_site/settings.py
+++ swiss_unity_league_site/settings.py
@@ -35,7 +35,7 @@ from django.contrib.messages import constants as messages
 import cid.locals
 from prometheus_client import Info
 
-from championship.season import SEASON_2024
+from championship.season import SEASON_2025
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -307,8 +307,8 @@ BLEACH_ALLOWED_TAGS = [
 BLEACH_ALLOWED_TAGS_ARTICLE = BLEACH_ALLOWED_TAGS + ["h2", "h3", "h4"]
 
 
-INFO_TEXT_DEFAULT_SEASON = SEASON_2024
-DEFAULT_SEASON = SEASON_2024
+INFO_TEXT_DEFAULT_SEASON = SEASON_2025
+DEFAULT_SEASON = SEASON_2025
 
 CACHES = {
     "default": {
