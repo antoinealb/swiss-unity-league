128a7d91661f3129fd6c3b1a38ed312040d1084d
Antoine Albertelli <antoinealb@google.com>
2024-12-29 19:46:26 +0100

    Cache geocoding results
    If we end up in a case where we often convert Addresses to their
    geocoding equivalent, we want to make sure it does not blow up our
    geocoding API expenses.

--- geo/address.py
+++ geo/address.py
@@ -12,13 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
 from collections import ChainMap
+from datetime import timedelta
 
 from django.conf import settings
 from django.utils.module_loading import import_string
 
 from geopy.exc import GeocoderServiceError
-from geopy.geocoders.base import Geocoder
+from geopy.geocoders.base import Geocoder, Point
 from prometheus_client import Histogram
 from tenacity import (
     retry,
@@ -26,6 +28,8 @@ from tenacity import (
     stop_after_delay,
     wait_exponential_jitter,
 )
+
+from championship.cache_function import cache_function
 
 geocode_duration = Histogram(
     "geocode_latency_seconds", "Duration of a call to the geocoding backend"
@@ -48,20 +52,31 @@ def _make_geocoder() -> Geocoder:
     return GeocoderClass(user_agent=user_agent, timeout=2, **kwargs)
 
 
-@geocode_duration.time()
+@cache_function(
+    # We use a digest to remove non-ascii characters
+    cache_key=lambda s: hashlib.md5(s.encode()).hexdigest(),
+    # Geocoding results are expensive and unlikely to change, keep them for
+    # long.
+    cache_ttl=timedelta(days=31).total_seconds(),
+)
+@geocode_duration.time()  # measure geocoding latency, including retries
 @retry(
     stop=stop_after_delay(5),
     wait=wait_exponential_jitter(initial=0.05, max=2),
     retry=retry_if_exception_type(GeocoderServiceError),
     reraise=True,
 )
-def address_to_coordinate(addr) -> tuple[float, float]:
+def _geocoder_query_cached(addr: str) -> Point | None:
     coder = _make_geocoder()
+    return coder.geocode(addr)
 
+
+def address_to_coordinate(addr) -> tuple[float, float]:
     address_parts = [
         addr.street_address,
         f"{addr.postal_code} {addr.city}",
         addr.country.name,
     ]
-    location = coder.geocode(", ".join(address_parts))
+
+    location = _geocoder_query_cached(", ".join(address_parts))
     return location.latitude, location.longitude
